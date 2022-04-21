8a1385d44031a4f80db55b35e41aa60a6c14e975
Josemaria Joaquin Floro <qfloro@josemarias-macbook-pro.home>
2022-04-21 14:44:17 +0200

    Finish To Do List V1 with Node Date Module

--- app.js
+++ app.js
@@ -2,39 +2,47 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    let today = new Date();
+app.get("/", (req, res) => {    
 
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
+    const day = date.getDate();
 
-    let day = today.toLocaleDateString("en-US", options);
-
-    res.render("list", { kindofDay: day, newListItems: items});
+    res.render("list", { listTitle: day, newListItems: items });
 
 });
 
+app.post("/", (req, res) => {
 
-app.post("/", (req,res) => {
-    let item = req.body.newItem;
-    items.push(item);
+    const item = req.body.newItem;
 
-    res.redirect("/");
-})
+    if(req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+});
 
+app.get("/work", (req, res) => {
+    res.render("list", {listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", (req,res) => {
+    res.render("about");
+});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
@@ -47,49 +55,4 @@ app.listen(3000, function () {
 
 
 
-    // var currentDay = today.getDay();
-    // var day = ""
-
-    // switch (currentDay) {
-    //     case 0:
-    //         day = "Sunday"
-    //         break;
-    //     case 1:
-    //         day = "Monday"
-    //         break;
-    //     case 2:
-    //         day = "Tuesday"
-    //         break;
-    //     case 3:
-    //         day = "Wednesday"
-    //         break;
-    //     case 4:
-    //         day = "Thursday"
-    //         break;
-    //     case 5:
-    //         day = "Friday"
-    //         break;
-    //     case 6:
-    //         day = "Saturday"
-    //         break;
-    //     default:
-    //         console.log("Error: current day is equal to: " + currentDay);
-
-    // }
-
-    // if (currentDay === 0) {
-    //     day = "Sunday";
-    // } else if (currentDay === 1) {
-    //     day = "Monday";
-    // } else if (currentDay === 2) {
-    //     day = "Tuesday";
-    // } else if (currentDay === 3) {
-    //     day = "Wednesday";
-    // } else if (currentDay === 4) {
-    //     day = "Thursday";
-    // } else if (currentDay === 5 ) {
-    //     day="Friday"
-    // }
-  
-
-
+    
